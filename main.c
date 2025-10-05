314a771eb5e0e18ce85fe37df400e2df65a578d4
Joao <joaovictormorais@yahoo.com.br>
2025-10-05 20:09:21 -0300

    adding solve function

--- main.c
+++ main.c
@@ -14,22 +14,16 @@ int ReadValidateMaze(const char *filename, int *width, int *height) {
         return 0;  
     }
 
-
     int row = 0;           
     int col = 0;          
     int first_width = -1;  
     char c;                
 
     while ((c = fgetc(file)) != EOF) {
-        
-        if (c == '\r') {
-            continue;
-        }
+        if (c == '\r') continue;
 
         if (c == '\n') {
-            if (first_width == -1) {
-                first_width = col;
-            }
+            if (first_width == -1) first_width = col;
             else if (col != first_width && col > 0) {
                 fclose(file);
                 return 0;  
@@ -47,15 +41,12 @@ int ReadValidateMaze(const char *filename, int *width, int *height) {
             return 0;
         }
 
-
         maze[row][col] = c;
         col++;
     }
 
     if (col > 0) {
-        if (first_width == -1) {
-            first_width = col;
-        }
+        if (first_width == -1) first_width = col;
         else if (col != first_width) {
             fclose(file);
             return 0;
@@ -68,18 +59,42 @@ int ReadValidateMaze(const char *filename, int *width, int *height) {
     *width = first_width;
     *height = row;
 
-    if (*width < 1 || *height < 1) {
-        return 0;
-    }
+    if (*width < 1 || *height < 1) return 0;
 
-    if (maze[0][0] == 'X' || maze[*height - 1][*width - 1] == 'X') {
-        return 0;
-    }
+    if (maze[0][0] == 'X' || maze[*height - 1][*width - 1] == 'X') return 0;
 
     return 1;  
 }
 
-// function to print the maze
+// Recursive function to solve the maze
+int solveMaze(int x, int y, int width, int height) {
+    if (x < 0 || y < 0 || x >= height || y >= width)
+        return 0;
+
+    if (maze[x][y] == 'X' || maze[x][y] == '.')
+        return 0;
+
+    if (x == height - 1 && y == width - 1) {
+        maze[x][y] = 'o';
+        return 1;
+    }
+
+    // Mark as visited
+    maze[x][y] = '.';
+
+    // Try moving: down, right, up, left
+    if (solveMaze(x + 1, y, width, height) ||
+        solveMaze(x, y + 1, width, height) ||
+        solveMaze(x - 1, y, width, height) ||
+        solveMaze(x, y - 1, width, height)) {
+        maze[x][y] = 'o'; 
+        return 1;
+    }
+
+    return 0; 
+}
+
+// Function to print the maze
 void printMaze(int width, int height) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -102,7 +117,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printMaze(width, height);
+    if (solveMaze(0, 0, width, height)) {
+        printMaze(width, height);
+    } else {
+        printf("no solution found\n");
+    }
 
     return 0;
 }
